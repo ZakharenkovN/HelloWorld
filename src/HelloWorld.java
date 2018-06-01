7951ad393ac483d97c8096efbcfbbe7786d8f405
Zakharenkov Nikolay <znpost@mail.ru>
2018-06-01 16:44:46 +0700

    commit из idea + замена создания объекта Runnable лямбда-выражением. Еле написал) Х.з воще че это такое. Потом идея заменила на какую-то херню "MethodReference"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+
+class HelloSwing {
+    HelloSwing() {
+        JFrame jFrm = new JFrame("Hello World");
+        jFrm.setSize(375, 150);
+        jFrm.setLocation(496, 309);
+        jFrm.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JLabel jLab = new JLabel("Текс в окошке. Hello world again");
+        //jLab.setLocation((jFrm.getWidth() / 2) - jLab.getWidth(), (jFrm.getHeight() / 2) - jLab.getHeight());
+        jFrm.add(jLab);
+        jFrm.setVisible(true);
+    }
+}
+
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(HelloSwing::new);
+    }
 }
