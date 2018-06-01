ae800f3b80b245b9e020b2ba0631e57e5eac2834
Zakharenkov Nikolay <znpost@mail.ru>
2018-06-01 17:04:57 +0700

    Трым бым бым

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,7 +8,7 @@ class HelloSwing {
         jFrm.setLocation(496, 309);
         jFrm.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JLabel jLab = new JLabel("Текс в окошке. Hello world again");
-        //jLab.setLocation((jFrm.getWidth() / 2) - jLab.getWidth(), (jFrm.getHeight() / 2) - jLab.getHeight());
+        jLab.setLocation((jFrm.getWidth() / 2) - jLab.getWidth(), (jFrm.getHeight() / 2) - jLab.getHeight());
         jFrm.add(jLab);
         jFrm.setVisible(true);
     }
